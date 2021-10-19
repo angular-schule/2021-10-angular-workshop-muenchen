63183f233077cab7d183fa1ae563884f137ae355
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-19 13:15:02 +0200

    Interface Book

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  books: Book[] = [];
 
-  ngOnInit(): void {
+  constructor() {
+    this.books = [
+      {
+        isbn: '000',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        rating: 5,
+        price: 36.9
+      },
+      {
+        isbn: '111',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        rating: 3,
+        price: 32.9
+      }
+    ];
   }
+
+  ngOnInit(): void {}
 
 }
