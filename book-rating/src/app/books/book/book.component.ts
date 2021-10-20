dcb7e7437f37766c35387f8334a5b73e9ee6363a
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-20 11:51:35 +0200

    Event Bindings

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 /**
@@ -16,14 +16,25 @@ import { Book } from '../shared/book';
 })
 export class BookComponent implements OnInit {
 
+  @Output() rateUp = new EventEmitter<Book>(); // from @angular/core
+  @Output() rateDown = new EventEmitter<Book>();
+
   @Input() book?: Book;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {}
 
   getStars() {
     return new Array(this.book?.rating);
+  }
+
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown() {
+    this.rateDown.emit(this.book);
   }
 
 }
