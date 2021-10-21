ad81a4bc78463e4a107f6ab848fdb00c750f1fd1
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-21 10:34:24 +0200

    Test EventEmitter

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { BookComponent } from './book.component';
 
@@ -16,10 +18,35 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3,
+      price: 4
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event for doRateUp()', () => {
+    // Arrange
+    let emittedBook: Book | undefined;
+    component.rateUp.subscribe(book => {
+      emittedBook = book;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(emittedBook).toBeTruthy();
+    expect(emittedBook).toBe(component.book);
+
   });
 });
