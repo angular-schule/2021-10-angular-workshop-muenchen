4a773be05d9efb0c3c8d28fd67095790ef4f9041
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-22 13:01:28 +0200

    Routing: Vorbereitung für Suche & Formulare

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -6,12 +6,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookSearchComponent } from './book-search/book-search.component';
+import { BookCreateComponent } from './book-create/book-create.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    BookSearchComponent,
+    BookCreateComponent
   ],
   imports: [
     CommonModule,
