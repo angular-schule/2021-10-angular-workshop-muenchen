4fad272f6b8af9ea983ab0d38787429615508398
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-22 12:02:54 +0200

    Routing: Lazy Loading

--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'books', component: DashboardComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent }
+  { path: '', component: DashboardComponent },
+  { path: ':isbn', component: BookDetailsComponent },
 ];
 
 @NgModule({
