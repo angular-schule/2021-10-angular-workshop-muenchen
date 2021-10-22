9f5b2dfc868b892247c4231934d38beba7f54406
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-22 10:34:31 +0200

    Routing: Parameter auslesen

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'br-book-details',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    // Synchroner Weg (PULL)
+    // const isbn = this.route.snapshot.paramMap.get('isbn');
+    // console.log(isbn);
+
+    // Asynchroner Weg (PUSH)
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn');
+      console.log(isbn);
+    });
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+/*
+  - Redirect ✅
+  - Links ✅
+  - Detailseite
+    - ISBN lesen
+    - HTTP
+    - Anzeige/Template
+*/
