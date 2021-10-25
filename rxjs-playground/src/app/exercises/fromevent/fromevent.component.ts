35629e101167d7efb82bda49079f2842edb8b29e
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-25 14:20:37 +0200

    RxJS: fromEvent, debounceTime, startWith

--- rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
+++ rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
@@ -22,7 +22,18 @@ export class FromeventComponent {
 
     /******************************/
 
-    
+    const width$ = fromEvent(window, 'resize').pipe(
+      debounceTime(1000),
+      map(() => window.innerWidth),
+      startWith(window.innerWidth)
+    );
+
+
+    width$.subscribe(width => {
+      this.currentWidth = width;
+    })
+
+
     /******************************/
   }
 
