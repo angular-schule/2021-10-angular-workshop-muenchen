9ac9c275356a55478bc612cff8673afe21948fa1
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-26 12:05:59 +0200

    AsyncPipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -4,6 +4,7 @@ import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
 import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-book-details',
@@ -12,7 +13,7 @@ import { map, switchMap } from 'rxjs/operators';
 })
 export class BookDetailsComponent implements OnInit {
 
-  book?: Book;
+  book$: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
     // Synchroner Weg (PULL)
@@ -20,12 +21,10 @@ export class BookDetailsComponent implements OnInit {
     // console.log(isbn);
 
     // Asynchroner Weg (PUSH)
-    this.route.paramMap.pipe(
+    this.book$ = this.route.paramMap.pipe(
       map(params => params.get('isbn')!),
       switchMap(isbn => this.bs.getSingle(isbn))
-    ).subscribe(book => {
-      this.book = book;
-    });
+    );
 
     /*
     Aufgabe: Buch abrufen und anzeigen
